cd6ba00260d8a3d10bca534216ecf29e1314a4b0
Arthur Efremenko <aefremenko24@gmail.com>
2025-10-24 17:54:01 -0400

    Initial Commit

---
+++ HealthTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HealthTracker
+//
+//  Created by Arthur Efremenko on 10/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
